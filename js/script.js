8e0ec60d34424cb7e56bd7495d64b518235434a6
Fernando Pinhel <fernandopinhelll@gmail.com>
2020-02-27 16:20:00 -0300

    Add arquivos steps.

---
+++ js/script.js
@@ -0,0 +1,36 @@
+// script for tab steps
+$('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
+  var href = $(e.target).attr('href');
+  var curr = $(".process-model a[href='" + href + "']").parent();
+
+  $('.process-model li').removeClass();
+  curr.addClass("active");
+  curr.prevAll().addClass("visited");
+});
+
+$('.process-model li').click(function(){
+  $(".tab-pane").removeClass("animation-to-right");
+  $(".tab-pane").addClass("animation-to-left");
+});
+
+$('.process-model li.visited').click(function(){
+  $(".tab-pane").addClass("animation-to-right");
+  $(".tab-pane").removeClass("animation-to-left");
+});
+
+$('.process-model li.active').click(function(){
+  $(".tab-pane").removeClass("animation-to-left");
+  $(".tab-pane").addClass("animation-to-right");
+});
+
+$('button.next').click(function(){
+  $('.nav li.active').next('li').find('a').attr("data-toggle","tab").trigger("click");
+  $(".tab-pane").addClass("animation-to-left");
+  $(".tab-pane").removeClass("animation-to-right");
+});
+
+$('button.prev').click(function() {
+  $('.nav li.active').prev('li').find('a').attr("data-toggle","tab").trigger("click");
+  $(".tab-pane").addClass("animation-to-right");
+  $(".tab-pane").removeClass("animation-to-left");
+});
